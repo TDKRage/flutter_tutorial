09a60a6f94e978faa94a89476f01493c8b74a04a
Lyle Classen <lylec@globalkinetic.com>
2020-03-04 21:17:36 +0200

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,30 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'My First App1'
+          'My First App'
         ),
         centerTitle: true,
         backgroundColor: Colors.red[600] ,
     ),
-    body: Center(
-      child: IconButton(icon: Icon(Icons.autorenew), onPressed: null)
+    body: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Expanded(child: FlatButton(onPressed: null, child: Text('Click'))),
+        Expanded(
+            child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Sup bitch'),
+          ),
+        ),
+        Expanded(
+            child: Container(
+            color:Colors.cyan,
+            padding: EdgeInsets.all(30) ,
+            child: Text('Inside TExrt'),
+          ),
+        )
+      ],
     ),
     floatingActionButton: FloatingActionButton(
         child: Text('Click'),
